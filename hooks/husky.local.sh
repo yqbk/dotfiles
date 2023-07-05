440033920761bf8af3d15c3819239575ff29cd45
Jakub Syrek <jksyrek@gmail.com>
2023-07-05 11:00:20 +0200

    Add husky config.

---
+++ hooks/husky.local.sh
@@ -0,0 +1,6 @@
+# Created by Husky v4.3.8 (https://github.com/typicode/husky#readme)
+#   At: 02/01/2023, 10:58:33
+#   From: /Users/yqbk/code/fundingport-fe/node_modules/husky (undefined)
+
+packageManager=npm
+cd "."
